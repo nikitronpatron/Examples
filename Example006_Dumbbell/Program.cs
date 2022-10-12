8a812d99db4214a4cd04ddc0e6b4a012a44e9f73
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-12 17:28:52 +0300

    Example006_Dumbbell

---
+++ Example006_Dumbbell/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 1;
+int c = 2;
+int d = 4;
+int e = 7;
+int f = 5;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+
+Console.WriteLine("max = " + max);
