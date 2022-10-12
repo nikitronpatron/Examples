308918e1f93905d6d716693210b3a94f1e7b2fc6
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-12 17:16:05 +0300

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name: ");
+string UserName = Console.ReadLine();
+
+if(UserName.ToLower() == "katya" || UserName.ToLower() == "ekaterina")
+{
+    Console.WriteLine("Hello, " + UserName + "! I love you!");
+}
+else
+{
+    Console.WriteLine("Hi, " + UserName);
+}
