d1b52eb16a5f0697274303917697700f7ac96972
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 23:42:17 +0300

    Example012_WWA1

---
+++ Example012_WWA1/Program.cs
@@ -0,0 +1,28 @@
+﻿// WWA - work with array
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(10, 50);
+        index++;
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    int count = numbers.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(numbers[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
