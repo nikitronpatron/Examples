bbd1902806d4716fbb8acfcf8832aaa960bd5c44
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 21:18:32 +0300

    Exxample008_Lecture

---
+++ Example008_Lecture/Program.cs
@@ -0,0 +1,8 @@
+﻿// на входе - число, на выходе - квадрат
+Console.WriteLine("Enter a number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqr = number * number;
+
+Console.WriteLine($"The square of the {number} is equal {sqr}");
