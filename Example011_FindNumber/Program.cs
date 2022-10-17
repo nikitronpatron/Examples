8184da0935092dbd76c1a59b06e2e656a75050a6
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 23:24:37 +0300

    Example011_FindNumber

---
+++ Example011_FindNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = {53, 345, 564, 3242, 25, 6554, 435, 1, 25};
+
+int length = array.Length;
+int find = 25;
+int index = 0;
+
+while (index < length)
+{
+    // if (array[index] != 25) 
+    // {
+    //     index++;
+    // }
+    // else
+    // {
+    //     Console.WriteLine(index);
+    //     index++;
+    // }
+    if(array[index] == 25)
+    {
+        Console.WriteLine(index);
+        break; // Выведет только один, первый найденый индекс, из всех вохможных и прирвется
+    }
+    index++;
+}
