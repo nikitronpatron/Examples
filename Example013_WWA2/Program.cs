e695774b11617e7273a2f085ea9235729597a2e1
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-17 23:56:01 +0300

    Example013_WWA2

---
+++ Example013_WWA2/Program.cs
@@ -0,0 +1,50 @@
+﻿// WWA - work with array
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    int count = numbers.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(numbers[position]);
+        position++;
+    }
+}
+
+int IndexFind(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexFind(array, 5);
+Console.WriteLine(pos);
