caf20ca985fb3591c7f34666e84d6f6a032b2f62
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-18 04:49:47 +0300

    Example016_Lecture3

---
+++ Example016_Lecture3/Program.cs
@@ -0,0 +1,12 @@
+﻿int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (first % second == 0)
+{
+    Console.WriteLine($"Число {second} кратно числу {first}");
+}
+else
+{
+    int rest = first % second;
+    Console.WriteLine($"Число {second} не кратно числу {first}, остаток {rest}");
+}
