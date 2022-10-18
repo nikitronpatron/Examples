74bef2af59129defb421cc95b1a2e795f117baee
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-18 04:41:10 +0300

    Example015_Lecture2

---
+++ Example015_Lecture2/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(100, 999);
+Console.WriteLine(number);
+
+int one = number % 10;
+///int ten = number / 10;
+int hundred = number / 100;
+
+Console.WriteLine($"{hundred}{one}");
