8f83e59283097f76fabbc51c73a0f8d04f4b9073
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-18 04:33:47 +0300

    Example014_Lecture1

---
+++ Example014_Lecture1/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next(10, 99);
+
+int one = number % 10;
+int ten = number / 10;
+
+Console.WriteLine(number);
+
+if (one > ten)
+{
+    Console.WriteLine(one);
+}
+else
+{
+    Console.WriteLine(ten);
+}
