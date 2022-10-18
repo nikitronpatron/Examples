8abe93f6b804a71f8a7036d36fb313c80b262539
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-18 05:05:38 +0300

    Example018_Lecture5

---
+++ Example018_Lecture5/Program.cs
@@ -0,0 +1,12 @@
+﻿int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+
+if (first == second * second ||
+    second == first * first)
+{
+    Console.WriteLine($"{first}, {second} -> yes");
+}
+else
+{
+    Console.WriteLine($"{first}, {second} -> no");
+}
