8111025423c31181bcb0e13681fc916c497dd856
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-23 19:17:55 +0300

    Example019_Lecture6

---
+++ Example019_Lecture6/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int GetQuarterFromCoordinate (int userX, int userY)
+{
+    int quarter = 0;
+
+    if (userX > 0 && userY > 0)
+        quarter = 1;
+    if (userX > 0 && userY < 0)
+        quarter = 4;
+    if (userX < 0 && userY < 0)
+        quarter = 3;
+    if (userX < 0 && userY > 0)
+        quarter = 2;
+
+    return quarter;
+}
+
+Console.WriteLine("Enter coordinates: ");
+int userX = Convert.ToInt32(Console.ReadLine());
+int userY = Convert.ToInt32(Console.ReadLine());
+int quarter = GetQuarterFromCoordinate(userX, userY);
+Console.WriteLine($"You are in ({userX}, {userY}), in {quarter} quarter");
