24b32bc368bf3aefb8b2f4066749a3e2e830b5d4
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-23 20:59:00 +0300

    Example020_Lecture7

---
+++ Example020_Lecture7/Program.cs
@@ -0,0 +1,36 @@
+﻿string GetCoordinatesFromQuarter(int userQuarter)
+{   
+    string coordinates = "";
+
+    switch (userQuarter)
+    {
+        case 1:
+            Console.WriteLine("X belongs from zero to infinity and Y belongs from zero to infinity");
+            break;
+        case 2:
+            Console.WriteLine("X belongs from zero to minus infinity and Y belongs from zero to infinity");
+            break;
+        case 3:
+            Console.WriteLine("X belongs from zero to minus infinity and Y belongs from zero to minus infinity");
+            break;
+        case 4:
+            Console.WriteLine("X belongs from zero to infinity and Y belongs from zero to minus infinity");
+            break;
+    }
+    
+    return coordinates;
+}
+
+Console.WriteLine("Enter a quarter ");
+int userQuarter = Convert.ToInt32(Console.ReadLine());
+if (userQuarter > 0 && userQuarter < 5)
+{
+    Console.Write($"In {userQuarter} quarter ");
+    string coordinates = GetCoordinatesFromQuarter(userQuarter);
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Error");
+    Console.ResetColor();
+}
