5894342f2c4af4bcd3a47bc6f34e653dfcf037ce
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-23 21:58:12 +0300

    Example021_Lecture8

---
+++ Example021_Lecture8/Program.cs
@@ -0,0 +1,12 @@
+﻿void SquareIt (int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        int square = i * i;
+        Console.Write($"{square}    ");
+    }
+}
+
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+SquareIt(number);
