9dea8fb7f618f6ae56db98226f727bcdf1845f3e
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-23 22:40:48 +0300

    Example022_Lecture9

---
+++ Examples022_Lecture9/Program.cs
@@ -0,0 +1,13 @@
+﻿double FindInterval (double aX, double bX, double aY, double bY)
+{
+    double interval = Math.Sqrt((bX - aX) * (bX - aX) + (bY - aY) * (bY - aY));
+    return interval;
+}
+Console.WriteLine("Enter coordinates of the point A: ");
+double aX = Convert.ToDouble(Console.ReadLine());
+double aY = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter coordinates of the point B: ");
+double bX = Convert.ToDouble(Console.ReadLine());
+double bY = Convert.ToDouble(Console.ReadLine());
+double interval = FindInterval(aX, bX, aY, bY);
+Console.WriteLine($"interval between points equal {interval}");
