0f6ff0c3f95663cdc522ef83726deeb5e727f68e
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-10 21:00:08 +0300

    Example029_Lecture12

---
+++ Example029_Lecture12/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
+
+int[] array = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join(",", array)}] ");
