f0baf70a62d292a125700d9c86b0cd2373772854
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-10 20:43:41 +0300

    Example028_Lecture11

---
+++ Example028_Lecture11/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int numb = Convert.ToInt32(Console.ReadLine());
+
+int LengthNumb(int number)
+{
+    int length = 0;
+    while(number > 0)
+    {
+        length++;
+        number = number / 10;
+    }
+    return length;
+}
+
+int num = LengthNumb(numb);
+Console.WriteLine(num);
