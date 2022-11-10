f97bdfb6cf0deb531c504c3e108618cdf87f4f34
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-10 19:52:13 +0300

    Example027_Lecture10

---
+++ Example027_Lecture10/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Result(int numb)
+{
+    int result = 0;
+    for(int i = numb; i > 0; i--)
+    {
+        result = result + i;
+        //Console.WriteLine(result);
+    }
+    return result;
+}
+
+int number = Result(num);
+Console.WriteLine(number);
