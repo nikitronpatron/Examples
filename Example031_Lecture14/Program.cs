e28b58276ac7263f8d23add1e21d0ac8a20701b9
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-14 18:23:22 +0300

    Example031_Lecture14

---
+++ Example031_Lecture14/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ShowArrayNegative(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(5, -100, 100);
+ShowArray(myArray);
+ShowArrayNegative(myArray);
+
