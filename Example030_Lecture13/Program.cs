b9635ded5cfedbc679db2ee494e858526f988a3c
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-14 14:19:13 +0300

    Example030_Lecture13

---
+++ Example030_Lecture13/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(12, -9, 9);
+ShowArray(myArray);
+
+int sumPos = 0;
+int sumNeg = 0;
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] >= 0)
+    {
+        sumPos += myArray[i];
+    }
+    else
+    {
+        sumNeg += myArray[i];
+    }
+}
+
+System.Console.WriteLine($"positive = {sumPos}, negative = {sumNeg}");
