287f57f6fc3617cfd2cf4345c535d1f547435c11
nikitronpatron <nikita.borsh9876@gmail.com>
2022-11-20 23:53:49 +0300

    Example037_Lecture18

---
+++ Example037_Lecture18/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(10, 1, 15);
+ShowArray(myArray);
+
+void InverseArray(int[] array)
+{
+    int[] inverseArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        inverseArray[i] = array[array.Length - 1 - i];
+        Console.Write($"{inverseArray[i]} ");
+    }
+    Console.WriteLine();
+}
+InverseArray(myArray);
